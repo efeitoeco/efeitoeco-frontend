ac400c618fdabe2cf8338cf106aeb56c8d93ec18
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-04-06 02:14:40 -0300

    Adicionando filtros de categoria na tela de produtos

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -13,9 +13,11 @@ export class ProdutosComponent implements OnInit {
 
   produto: Produto = new Produto();
   listaProduto: Produto[];
+  listaProdutoFiltrada: Produto[];
 
   listaCategoria: Categoria[];
 
+  categoriasSelecionadas: number[];
 
   constructor(
     private produtoService: ProdutoServiceService,
@@ -25,12 +27,16 @@ export class ProdutosComponent implements OnInit {
   ngOnInit(){
     this.verTodosProdutos();
     this.verTodasCategorias();
+
+    this.listaProdutoFiltrada = this.listaProduto;
     
+    this.categoriasSelecionadas = new Array<number>();
   }
 
   verTodosProdutos() {
     this.produtoService.getAllProduto().subscribe((resp: Produto[]) => {
       this.listaProduto = resp;
+      this.listaProdutoFiltrada = resp;
     })
   }
 
@@ -38,5 +44,30 @@ export class ProdutosComponent implements OnInit {
     this.categoriaService.getAllCategoria().subscribe((resp: Categoria[]) =>{
       this.listaCategoria = resp;
     })
+  }
+
+  filtroCategoria(obj: Produto) {
+    return this.categoriasSelecionadas.includes(obj.categoria.id);
+  }
+
+  filtrarPorCategoria(event: any, id: number) {
+
+    if(event.target.checked) {
+      console.log(id + " checked");
+      this.categoriasSelecionadas.push(id);
+    } else {
+      console.log(id + " unchecked");
+      this.categoriasSelecionadas = this.categoriasSelecionadas.filter(m => m != id);
+    }
+
+    console.log(this.categoriasSelecionadas);
+    if(this.categoriasSelecionadas.length < 1) {
+      this.listaProdutoFiltrada = [...this.listaProduto];
+    } else {
+      this.listaProdutoFiltrada = this.listaProduto.filter((produto: Produto) => {
+        return this.categoriasSelecionadas.includes(produto.categoria.id);
+      })
+  }
+    console.log(this.listaProduto);
   }
 }
