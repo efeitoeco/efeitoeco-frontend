22137427ef290d0327dafb5eae501f29717dc2c1
Daniel Santos <daniel@Daniels-MacBook-Air.local>
2021-04-07 14:37:45 -0300

    pequenos ajustes

--- src/app/meus-produtos/meus-produtos.component.ts
+++ src/app/meus-produtos/meus-produtos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Produto } from '../models/Produto';
+import { Usuario } from '../models/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-meus-produtos',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeusProdutosComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario =  new Usuario();
+  meuId: number = environment.id;
+  listaDeProdutos: Produto[];
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService
+  ) { }
+
+  ngOnInit() {
+    console.log(this.meuId)
+    this.findByIdUsuario()
+    console.log(this.usuario.produtosVenda)
+    console.log(this.usuario.nome)
+
+    //this.listaDeProdutos = [...this.usuario.produtosVenda]
+    this.preencherProdutos()
+    console.log(this.listaDeProdutos)
+  }
+
+  preencherProdutos() {
+    for(let i=0;i<this.usuario.produtosVenda.length;i++){
+     this.listaDeProdutos.push(this.usuario.produtosVenda[i])
+    }
+  }
+
+  findByIdUsuario() {
+    this.auth.getByIdUsuario(this.meuId).subscribe((resp: Usuario) => {
+      this.usuario = resp;
+    })
   }
 
 }
