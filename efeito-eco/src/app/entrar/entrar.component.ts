afbc708f1bd38864cf956ff27626d7e7c48296d5
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-04-10 03:08:22 -0300

    Modificando nav, começando carrosel de cards

--- efeito-eco/src/app/entrar/entrar.component.ts
+++ efeito-eco/src/app/entrar/entrar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { UsuarioLogin } from '../models/UsuarioLogin';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -16,7 +17,8 @@ export class EntrarComponent implements OnInit {
 
   constructor(
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit(){
@@ -41,9 +43,7 @@ export class EntrarComponent implements OnInit {
       this.router.navigate(['/home']);
 
     }, err=>{
-      if(err.status === 500){
-        alert('Amigo tu errou em algo em')
-      }
+      this.alertas.showAlertDanger("E-mail ou senha incorretos")
     })
   }
 
